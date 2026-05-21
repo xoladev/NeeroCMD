26a0bac24a9bba2742704a0f6968a699977f90a5
xoladev <ngiveros@gmail.com>
2026-05-21 12:13:36 +0700

    Update debugger.bat

--- resources/debugger.bat
+++ resources/debugger.bat
@@ -1,18 +1,20 @@
 @echo off
-if "%~1"=="" exit
-chcp 1251 >nul
+setlocal EnableExtensions
 title Debugger
-set PIPE_FILE=%CD%\temp\NeeroCMD_DbgTransfer.tmp
-if exist %PIPE_FILE% (
-  del "%PIPE_FILE%" >nul
-)
-powershell -Command ^
-  "Write-Host 'Open: NeeroCMD.lnk to start debugging it!' -f Blue; " ^
-  "Write-Host 'Debbuger started. Waiting for any data...' -f Green"
+
+set "PIPE_FILE=%~dp0temp\NeeroCMD_DbgTransfer.tmp"
+
+if not exist "%~dp0temp\" mkdir "%~dp0temp\" 2>nul
+if exist "%PIPE_FILE%" del /f /q "%PIPE_FILE%" >nul 2>&1
+
+powershell -NoProfile -Command ^
+  "Write-Host 'Open NeeroCMD.lnk to start the shell.' -ForegroundColor Blue; " ^
+  "Write-Host 'Debugger started. Waiting for log data...' -ForegroundColor Green"
 
 :loop
 if exist "%PIPE_FILE%" (
-    type %PIPE_FILE%
-    del "%PIPE_FILE%" >nul 2>&1
+    type "%PIPE_FILE%"
+    del /f /q "%PIPE_FILE%" >nul 2>&1
 )
+timeout /t 1 /nobreak >nul
 goto loop
